0c75fca24091f4e86921d996f51b3a971c02bd56
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-05-19 14:41:53 -0400

    added menu prompts

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password
+      password: 'root',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
+  connection.connect(function (err) {
+    if (err) throw err;
+  });
+  
+  module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name_)
+VALUES ("JavaHall");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("juniorDev", 120000, 1);
+
+INSERT INTO employee (first_name, last_name, manager_id)
+VALUES ("Fernando", "Martinez", 145)
