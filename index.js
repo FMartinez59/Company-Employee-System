05a9150b5364119b7c12f9d6eb8f065de4eca24b
Fernando Martinez Jr <fernando.martinez.jr.59@gmail.com>
2022-05-19 16:01:11 -0400

    added switch statements and functions for them

--- index.js
+++ index.js
@@ -1,50 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const connection = require("./db/connection");
-
-function menuPrompts() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "selections",
-      message: "What would you like to do?",
-      choices: [
-        {
-          name: "View All Departments",
-          value: "VIEW_DEPARTMENTS",
-        },
-        {
-          name: "View All Roles",
-          value: "VIEW_ROLES",
-        },
-        {
-          name: "View All Employees",
-          //might need to change based on db name
-          value: "VIEW_EMPLOYEES",
-        },
-        {
-          name: "Add a Department",
-          value: "ADD_DEPARTMENT",
-        },
-        {
-          name: "Add a Role",
-          value: "ADD_ROLE",
-        },
-        {
-          name: "Add an Employee",
-          value: "ADD_EMPLOYEE",
-        },
-        {
-          name: "Quit",
-          value: "QUIT",
-        },
-      ],
-    },
-  ]).then (res => {
-
-  })
-}
-
 //switch
 // connection.query('SELECT * FROM roles', function (err, results) {
 //     console.log(results);
@@ -53,3 +9,85 @@ function menuPrompts() {
 //   connection.query('INSERT INTO employee (first_name, last_name, manager_id) VALUES ("David", "Johnson", 146)', function (err, results) {
 //     console.log(results);
 //   });
+function menuPrompts() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "selections",
+        message: "What would you like to do?",
+        choices: [
+          {
+            name: "View All Departments",
+            value: "VIEW_DEPARTMENTS",
+          },
+          {
+            name: "View All Roles",
+            value: "VIEW_ROLES",
+          },
+          {
+            name: "View All Employees",
+            //might need to change based on db name
+            value: "VIEW_EMPLOYEES",
+          },
+          {
+            name: "Add a Department",
+            value: "ADD_DEPARTMENT",
+          },
+          {
+            name: "Add a Role",
+            value: "ADD_ROLE",
+          },
+          {
+            name: "Add an Employee",
+            value: "ADD_EMPLOYEE",
+          },
+          {
+            name: "Quit",
+            value: "QUIT",
+          },
+        ],
+      },
+    ])
+    .then((res) => {
+      let selections = res.selections;
+      switch (selections) {
+        case "VIEW_DEPARTMENTS":
+          viewDepartments();
+          break;
+        case "VIEW_ROLES":
+          viewRoles();
+          break;
+        case "VIEW_EMPLOYEES":
+          viewEmployees();
+          break;
+        case "ADD_DEPARTMENT":
+          addDepartment();
+          break;
+        case "ADD_ROLE":
+          addRole();
+          break;
+        case "ADD_EMPLOYEE":
+          addEmployee();
+          break;
+        default:
+          quit();
+      }
+    });
+}
+
+function viewDepartments() {}
+
+function viewRoles() {}
+
+function viewEmployees() {}
+
+function addDepartment() {}
+
+function addRole() {}
+
+function addEmployee() {}
+
+function quit() {
+  process.exit();
+}
